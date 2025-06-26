c3a6d7df0efd7f8a562e5dad43a2561c38646d75
Sharath Sivakumar <sharathsivakumar10@gmail.com>
2025-06-26 11:44:23 +0530

    Implement private routes and authentication checks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-// src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    <div>
-      <h1>Hello from React App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
